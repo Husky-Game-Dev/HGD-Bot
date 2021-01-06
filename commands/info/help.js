86dd8299a0f51ad53c4ba264f7fddf97b310fd39
Hao Duong <howarddful123@gmail.com>
2021-01-06 01:08:08 -0800

    help command now show a list of available commands

--- commands/info/help.js
+++ commands/info/help.js
@@ -8,9 +8,15 @@ module.exports = {
     if(!args.length) {
       const helpEmbed = new MessageEmbed()
       .setThumbnail(client.user.avatarURL())
+      .setTitle('Commands list')
       .setAuthor('Husky Game Dev', 'https://i.imgur.com/xMskFdp.png', 'https://huskygamedev.com')
       .setColor('RANDOM')
-      .setDescription('Use `!husky help [command]` to get more help!');
+      .setDescription('Use `!husky help [command]` to get more help!')
+      .addFields(
+        { name: 'Info', value: '`help` `TBD` `TBD` `TBD` `TBD`', inline: true },
+        { name: 'Server', value: '`kick` `ban` `TBD` `TBD` `TBD`', inline: true },
+        { name: 'Fun', value: '`gif` `presidential` `trumptweet` `rps`', inline: true },
+      );
       return message.channel.send(helpEmbed);
     }
     // !husky command arg2 arg3...
